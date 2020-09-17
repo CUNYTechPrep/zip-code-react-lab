308921b826da176e511f7d110d30b6d3bf452812
Edgardo Molina <medgardo@gmail.com>
2020-09-17 12:54:40 -0400

    Add starter code for the Zip Code Search App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+
+function City(props) {
+  return (<div>This is the City component</div>);
+}
+
+function ZipSearchField(props) {
+  return (<div>This is the ZipSearchField component</div>);
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Zip Code Search</h2>
+        </div>
+        <ZipSearchField />
+        <div>
+          <City />
+          <City />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
